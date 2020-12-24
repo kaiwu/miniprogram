2e5d648a356da526a051b704e626dfd2a7d40308
kaiwu <kaiwu2004@gmail.com>
2020-12-24 19:17:20 +0800

    rename min.css to wxss, requires utility rename

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+rm -rf target/global-logging
+rm -rf target/task-temp-directory
+rm -rf target/.history*
+rm -rf target/scala-*
+rm -rf target/style
 find target/ -name classes | xargs rm -rf
 find target/ -name streams | xargs rm -rf
 find target/ -name *.map | xargs rm -rf
-rm -rf target/global-logging
-rm -rf target/task-temp-directory
-rm -f target/.history*
-rm -rf target/scala-*
+find target/ -type f -name '*.min.css' -exec rename 's/\.min\.css/\.wxss/' '{}' \;
